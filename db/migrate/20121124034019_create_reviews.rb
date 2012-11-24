01104441bdd51dc94db5531b91556b0fc93179d5
Michael Chang <thenewme91@gmail.com>
2012-11-23 23:45:54 -0500

    Fix migration

--- db/migrate/20121124034019_create_reviews.rb
+++ db/migrate/20121124034019_create_reviews.rb
@@ -1,12 +1,12 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.number :charity_id
+      t.integer :charity_id
       t.string :author
       t.string :title
       t.string :description
       t.string :name
-      t.number :rating
+      t.integer :rating
       t.boolean :recommend
 
       t.timestamps
