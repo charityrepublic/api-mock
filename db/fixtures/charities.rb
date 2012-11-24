19a289a154cd340390c2f6958b6681d55df8b7d0
Farwa Naqi <farwa.naqi@gmail.com>
2012-11-23 22:08:01 -0500

    Add fixture for charities.

---
+++ db/fixtures/charities.rb
@@ -0,0 +1,7 @@
+Charity.seed do |c|
+  c.id = 1
+  c.country = "Canada"
+  c.description = "KidsAbility provides rehabilitation services for children from Waterloo Region and Guelph Wellington."
+  c.location = "Waterloo, Ontario"
+  c.name = "Kidsability Centre for Child Development"
+end
