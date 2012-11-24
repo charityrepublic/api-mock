5e746149bb36a6fac09260560080c97cecd26898
Michael Chang <thenewme91@gmail.com>
2012-11-23 23:43:14 -0500

    charity-id => charity_id

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,3 @@
 class Review < ActiveRecord::Base
-  attr_accessible :author, :charity-id, :description, :name, :rating, :recommend, :title
+  attr_accessible :author, :charity_id, :description, :name, :rating, :recommend, :title
 end
--- db/migrate/20121124034019_create_reviews.rb
+++ db/migrate/20121124034019_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.number :charity-id
+      t.number :charity_id
       t.string :author
       t.string :title
       t.string :description
