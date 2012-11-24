bc2b6bf8322de0ba56cad91534d7d4f38cbf882b
Michael Chang <thenewme91@gmail.com>
2012-11-23 23:48:36 -0500

    Add fixture for reviews.

---
+++ db/fixtures/reviews.rb
@@ -0,0 +1,11 @@
+Review.seed do |r|
+  r.id = 1
+  r.author = 'thenewme91@gmail.com'
+  r.charity_id = 1
+  r.description = 'Helped with the computer program for kids, as well as shelving books.'
+  r.name = 'Michael Chang'
+  r.title = 'Lots to do!'
+  r.rating = 4.9
+  r.recommend = true
+end
+
