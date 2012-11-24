c2ec08a358ff92a46bc0cc0ae070ba5769e96d56
Farwa Naqi <farwa.naqi@gmail.com>
2012-11-23 21:02:31 -0500

    Scaffold a rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApiMock::Application.config.secret_token = 'ac6a9943c85392681c11fb7951bf4493e3642d50d9a640fa11d6f13a7bd8d2a2bbc59c4a3051bd99e223173b06f1349cc46523bc801f64b14c5cc5839f1f21fa'
